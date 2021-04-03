58c0a436d5ba9aad0d31c5f05667783d90c229fd
Artem Potapchick <49004382+ArtemPotapchick@users.noreply.github.com>
2021-04-03 21:30:22 +0300

    cleaned components & added 3 book

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
 import React from 'react';
 
-const firstBook = {
+const books = [{
+  id: 1,
   img: 'https://images-na.ssl-images-amazon.com/images/I/816ZcMz1R3L._AC_UL200_SR200,200_.jpg',
-  title: 'The Women of the Bible\n'
-    + '      Speak: The Wisdom of 16 Women and Their Lessons for Today',
+  title: 'The Women of the Bible',
   author: 'Shannon Bream',
-};
-const secondBook = {
+},
+{
+  id: 2,
   img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
   title: 'I Love You to the Moon and Back',
   author: 'Amelia Hepworth',
-};
+},
+{
+  id: 3,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81xlx1cQhPL._AC_UL200_SR200,200_.jpg',
+  title: 'On the House: A Washington',
+  author: 'John Boehner',
+},
+];
+
 
 const Book = (props) => {
   const { author, title, img } = props;
-
 
   return <article className='book'>
     <img src={img} alt=""/>
@@ -25,10 +33,12 @@ const Book = (props) => {
 };
 
 function App() {
+  // attribute, eventHandler =====  events
   return (
     <section className='booklist'>
-      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}/>
-      <Book author={secondBook.author} title={secondBook.title} img={secondBook.img}/>
+      {books.map(book => {
+        return <Book key={book.id} {...book}/>;
+      })}
     </section>
   );
 }
